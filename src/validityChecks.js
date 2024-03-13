cca9527af94fb4c26292a55473e8c95ada065a48
panshak <panshakeden@gmail.com>
2024-03-13 10:18:48 +0100

    adding methods

--- src/validityChecks.js
+++ src/validityChecks.js
@@ -1,8 +1,53 @@
-exports.checkApproveDoctorValidity = (data) => {
-   if(!data.approved){
-       return {success:false,msg:"NOT_APPROVED BY OWNER"}
+exports.checkApproveDoctorValidity = (owners, doctorAddress) => {
+   if(owners.includes(doctorAddress)){
+       return {success:true,msg:"DOCTOR APPROVED BY OWNER"}
    }else{
-       return {success:true,msg:"APPROVED"}
+       return {success:false,msg:"DOCTOR NOT APPROVED BY OWNER"}
+   }
+}
+
+exports.checkMedicalRecordAndHistoryValidity = (doctors, doctorAddress,permissions) => {
+   if(doctors.includes(doctorAddress)){
+       return {success:true,msg:"VALID DOCTOR"}
+
+   }else if(!doctors.includes(doctorAddress)){
+      return { success: false, msg: "INAVLID DOCTOR"};
+     }
+   else if(permissions.includes(doctorAddress)){
+      return { success: true, msg: "UPDATED BY DOCTOR"};
+     }
+     else if(!permissions.includes(doctorAddress)){
+      return { success: false, msg: "NO PERMISSION TO UPDATED BY DOCTOR" };
+     }
+   else {
+      return { success: false, msg: "INVALID DOCTOR"};
+  }
+}
+
+
+
+
+exports.checkOwnerApproval = (owners, doctorAddress) => {
+   if (owners.includes(doctorAddress)) {
+       return { success: true, msg: "Doctor approved by owner", doctorAddress };
+   } else {
+       return { success: false, msg: "Doctor not approved by owner", doctorAddress };
+   }
+}
+
+exports.checkValidDoctor = (doctors, doctorAddress) => {
+   if (doctors.includes(doctorAddress)) {
+       return { success: true, msg: "Valid doctor", doctorAddress };
+   } else {
+       return { success: false, msg: "Invalid doctor", doctorAddress };
+   }
+}
+
+exports.checkPermissionUpdate = (permissions, doctorAddress) => {
+   if (permissions.includes(doctorAddress)) {
+       return { success: true, msg: "Permission updated by doctor", doctorAddress };
+   } else {
+       return { success: false, msg: "Permission not updated by doctor", doctorAddress };
    }
 }
 
@@ -25,31 +70,3 @@ exports.checkApproveDoctorValidity = (data) => {
 
 
 
-
-
-
-
-
-// async function medicalHistoryRecords(_medicalHistory) {
-//     medicalHistory[msg_sender] = {
-//         ...medicalHistory[msg_sender],
-//         ...medicalHistory
-//     }
-
-//     const response = await fetch(rollup_server + "/notice", {
-//        method: "POST",
-//        headers: {
-//          "Content-Type": "application/json",
-//        },
-//        body: JSON.stringify(medicalHistory),
-//     });
-   
-//     if (!response.ok) {
-//        throw new Error(`Failed to add patient: ${response.statusText}`);
-//     }
-   
-//     const data = await response.json();
-//     console.log("patient added successfully:", data);
-//     return data;
-//    }
-   
